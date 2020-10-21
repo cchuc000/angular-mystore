7710cc0c9fedcf4338b0d97b46e2e188516b7875
cchuc000 <cristofer.chuc91@student.qcc.cuny.edu>
2020-10-21 14:45:14 -0400

    finished code

---
+++ src/app/my-resume/resume-edu/resume-edu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-edu',
+  templateUrl: './resume-edu.component.html',
+  styleUrls: ['./resume-edu.component.css']
+})
+export class ResumeEduComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
